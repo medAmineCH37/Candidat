6eeff9262d3a9c12cda1332c754bc75a11fe3d6b
Med Amine Chouria <79566268+medAmineCH37@users.noreply.github.com>
2025-05-09 16:08:42 +0100

    eurekat config

--- src/main/java/com/example/Candidat/CandidatApplication.java
+++ src/main/java/com/example/Candidat/CandidatApplication.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CandidatApplication {
 
